287b3081326dc3135a14825abd61e3513ebf88fe
juan.feris@globant.com <juan.feris@globant.com>
2020-05-06 17:37:12 -0500

    Added space complexity

---
+++ Big-O/Space-Complexity/05-sum.js
@@ -0,0 +1,8 @@
+function sum(arr) {
+  let total = 0;  // 1
+  for (let i = 0 /* 1 */; i < array.length; i++) {
+    total += arr[1];    
+  }
+
+  return total;
+} // O(1) space it's always the same no mather the input
