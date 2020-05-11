fddd1cf817eb62341354aea443469478a945ccba
juan.feris@globant.com <juan.feris@globant.com>
2020-05-11 15:30:10 -0500

    Refactoring

--- Problem-Solving/01-frequency-counters/02-anagram-challenge.js
+++ Problem-Solving/01-frequency-counters/02-anagram-challenge.js
@@ -7,37 +7,27 @@
 
 function validAnagram(string1, string2){
 
-  // if both strings are empty, return true
-  if(string1 === '' && string2 === ''){
-    return true;
-  }
-
   // return false it strings doen´t have the same length
   if(string1.length !== string2.length){
     return false;
   }
 
-  // create objects
-  let frequencieCounter1 = {};
-  let frequencieCounter2 = {};
+  // create lookup object
+  const lookup = {};
 
   // Store the letters of the first string in an object that counts them
   for (const key in string1) {
     let char = string1.charAt(key)
-    frequencieCounter1[char] = (frequencieCounter1[char] || 0) + 1;
+    lookup[char] = (lookup[char] || 0) + 1;
   }
 
   // Store the letters of the first string in an object that counts them
   for (const key in string2) {
     let char = string2.charAt(key)
-    frequencieCounter2[char] = (frequencieCounter2[char] || 0) + 1;
-  }
-
-  // compare the two objects to determine if they share the same letters 
-  // and frequencies and return the result
-  for (const key in frequencieCounter1) {
-    if (frequencieCounter1[key] !== frequencieCounter2[key]) {
+    if (!lookup[char]) {
       return false;
+    } else {
+      lookup[char] -= 1;
     }
   }
   return true;
