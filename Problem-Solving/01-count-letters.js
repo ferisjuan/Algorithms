461cbd3632e0ccfe7a2b302dd5204614c943f908
juan.feris@globant.com <juan.feris@globant.com>
2020-05-11 15:23:34 -0500

    Count letters

--- Problem-Solving/01-count-letters.js
+++ Problem-Solving/01-count-letters.js
@@ -3,21 +3,25 @@ function charCount(str) {
   result = {};
 
   // loop over string for each character
-  for (let i = 0; i < str.length; i++) {
-    var char = str[i].toLowerCase();
-
-    // if the char is a number/letter and is a key in the object, add one to the count
-    if (result[char] > 0) {
-      result[char]++;
+  for (var char of str) {
+    char = char.toLowerCase();
+    if (isAlphaNumeric(char)) {
+      result[char] = ++result[char] || 1;
     }
-    // if the char is a number/letter and is in the object, add it
-    else {
-      result[char] = 1;
-    }
-    // if character is something else (space, period, etc.) do nothing
   }
-  // return the object
+  
   return result;
+}
+
+function isAlphaNumeric(char) {
+  var code = char.charCodeAt(0);
+  if (
+    !(code > 47 && code < 58) &&
+    !(code > 64 && code < 91) &&
+    !(code >96 && code < 123)  ) {
+    return false;
+  }
+  return true;
 }
 
 console.log(charCount("Hola Juanca!"));
